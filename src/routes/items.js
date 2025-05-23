4adc2ac4f7955224d26c114179fde59bbf92eff6
Maximillionair <maxmillian.heim@outlook.com>
2025-05-23 09:07:24 +0200

    Update items.js

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const itemsController = require('../controllers/itemsController');
+const itemsController = require('../controllers/itemscontroller');
 
 router.get('/', itemsController.getAllItems);
 router.post('/', itemsController.createItem);
