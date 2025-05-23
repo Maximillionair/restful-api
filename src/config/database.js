159baf02895b83c5b69740c6b63711cdb89897bf
Maximillionair <maxmillian.heim@outlook.com>
2025-05-23 09:25:12 +0200

    rawr

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 exports.connect = async ()=> {
     console.log(process.env.NIGGA)
 
     try{
+        console.log(process.env.MONGODB_URI)
         await mongoose.connect(process.env.MONGODB_URI);
     } catch {
         console.log("not connected");
